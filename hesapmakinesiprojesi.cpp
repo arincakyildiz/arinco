96f08c1beffda723481aaf073d50945f8b0d45d5
Ahmet Arınç Akyıldız <138769120+arincakyildiz@users.noreply.github.com>
2023-07-19 15:13:31 +0300

    Add files via upload

---
+++ hesapmakinesiprojesi.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main(){
+	int islem;
+	int value;
+	int sayi1,sayi2;
+	
+	printf("Islemler\n1:Toplama(+)\n2:Cikarma(-)\n3:Carpma(x)\n4:Bolme(/)\n\n\n");
+	printf("Islemi Seciniz:");
+	scanf("%d",&islem);
+	printf("Sayi 1'i giriniz: ");
+	scanf("%d",&sayi1);
+	printf("Sayi 2'yi giriniz: ");
+	scanf("%d",&sayi2);
+	
+	switch(islem){
+		
+		case 1:
+			value=sayi1+sayi2;
+			printf("Sayilarin birbirleriyle toplami %d\n",value);
+			
+			break;
+		
+		case 2:	
+		if (sayi1>sayi2){
+			value=sayi1-sayi2;
+			printf("Sayilarin birbirleriyle farki %d\n",value);
+			
+			break;
+	}
+		else {
+			printf("Lutfen sayi1'i sayi2'den buyuk olacak sekilde girin:");	
+			
+			break;
+		}
+
+		case 3:
+			value=sayi1*sayi2;
+			printf("Sayilarin birbirleriyle carpimi %d\n: ",value);
+			
+			break;
+		case 4:
+			if(sayi2!=0){
+			
+			
+			value=sayi1/sayi2;
+			printf("Sayilarin birbirine bolumu %d\n: ",value);
+			
+			break;
+			}
+			
+		default:
+			printf("Gecersiz Islem!");
+			
+			break;
+			
+	}
+		
+	return 0;
+}
